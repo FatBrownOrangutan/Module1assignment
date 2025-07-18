80109c483e817cb46539a9c59c7cb41f7bf50437
FatBrownOrangutan <jesusochoaa2008@gmail.com>
2025-07-18 13:53:46 -0700

    Create AssignmentOne.java

---
+++ AssignmentOne.java
@@ -0,0 +1,11 @@
+package example.description;
+
+public class description {
+
+	public static void main(String args[]) {
+		System.out.print("CSC142 – Intro to Java"
+				+ "\none thing I learned was the static void main(String arg[]) is"
+				+ "\npretty much the engine that starts the command and the System.out.print"
+				+ "\nis the one that prints it.");
+	}
+}
